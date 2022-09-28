77fbb503cc5bcdc32231ca0fad6b6422433536f1
unknown <sadikul.i.raju@gmail.com>
2022-09-28 15:12:34 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+
 
 function App() {
   return (
     <div className="">
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
